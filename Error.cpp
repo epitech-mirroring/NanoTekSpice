78e35b468e2edfa86699a23fed3540691d01d1d8
Thomaltarix <thomas.boue@epitech.eu>
2024-02-07 12:51:26 +0100

    :goal_net: Add the static error handling functions in the Error class

--- Error.cpp
+++ Error.cpp
@@ -6,10 +6,38 @@
 */
 
 #include "Error.hpp"
+#include <regex>
+#include <fcntl.h>
+#include <unistd.h>
 
 nts::Error::Error(const std::string &msg) : _msg(msg) {}
 
 const char *nts::Error::what() const noexcept
 {
     return _msg.c_str();
+}
+
+void nts::Error::check_args_number(int argc)
+{
+    if (argc != 2)
+        throw nts::Error("Invalid number of arguments.");
+}
+
+void nts::Error::check_file_extension(char *filename)
+{
+    std::regex reg(".*\\.nts");
+    std::smatch match;
+    std::string str(filename);
+
+    if (!std::regex_search(str, match, reg))
+        throw nts::Error("Invalid file extension.");
+}
+
+void nts::Error::check_file_existence(char *filename)
+{
+    int fd = open(filename, O_RDONLY);
+
+    if (fd == -1)
+        throw nts::Error("File does not exist.");
+    close(fd);
 }
--- Error.hpp
+++ Error.hpp
@@ -15,6 +15,10 @@ namespace nts {
             Error(const std::string &msg);
             ~Error() = default;
             const char* what() const noexcept;
+
+            static void check_args_number(int argc);
+            static void check_file_extension(char *filename);
+            static void check_file_existence(char *filename);
         private:
             std::string _msg;
     };
