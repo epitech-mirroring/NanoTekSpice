dd319f32da297923945462a457e608bd96febfeb
RenardFute <axel.eckenberg@yahoo.fr>
2024-02-07 14:23:29 +0100

    :sparkles: Create component factory New component factory that allows you to register components and clone them back

--- src/IComponent.hpp
+++ src/IComponent.hpp
@@ -24,5 +24,6 @@ namespace nts {
             virtual void            setLink(std::size_t pin,
                                             nts::IComponent &other,
                                             std::size_t otherPin) = 0;
+            [[nodiscard]] virtual IComponent     *clone() const = 0;
     };
 }
