f2c8533ad5ae8385a2c01816758bd4c3db6fad40
RenardFute <axel.eckenberg@yahoo.fr>
2024-02-08 18:53:16 +0100

    :art: Use "using namespace" instead of nested "namespace"

--- src/components/InputComponent.cpp
+++ src/components/InputComponent.cpp
@@ -8,23 +8,23 @@
 
 #include "InputComponent.hpp"
 
-namespace nts::Components {
-    InputComponent::InputComponent(): AbstractComponent(1) {
-        _value = UNDEFINED;
-        this->setPinMode(1, PinMode::OUTPUT);
-    }
+using namespace nts::Components;
 
-    Tristate InputComponent::compute(std::size_t pin) {
-        if (pin != 1)
-            return UNDEFINED;
-        return _value;
-    }
+InputComponent::InputComponent(): AbstractComponent(1) {
+    _value = UNDEFINED;
+    this->setPinMode(1, PinMode::OUTPUT);
+}
 
-    void InputComponent::setValue(Tristate value) {
-        _value = value;
-    }
+nts::Tristate InputComponent::compute(std::size_t pin) {
+    if (pin != 1)
+        return UNDEFINED;
+    return _value;
+}
 
-    InputComponent *InputComponent::clone() const {
-        return new InputComponent();
-    }
+void InputComponent::setValue(Tristate value) {
+    _value = value;
+}
+
+InputComponent *InputComponent::clone() const {
+    return new InputComponent();
 }
