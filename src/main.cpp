30460d846de6a32121f3e5f0ecc10ca142f2699c
RenardFute <axel.eckenberg@yahoo.fr>
2024-02-19 14:38:27 +0100

    :sparkles: Add 'true' and 'false' in the factory

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,16 @@
 #include "components/InputComponent.hpp"
 #include "components/OrComponent.hpp"
 #include "components/Composed4071Component.hpp"
+#include "components/TrueComponent.hpp"
+#include "components/FalseComponent.hpp"
 
 static void registerComponents(nts::ComponentFactory &factory)
 {
     factory.registerComponent("input", new nts::Components::InputComponent());
     factory.registerComponent("or", new nts::Components::OrComponent());
     factory.registerComponent("4071", new nts::Components::Composed4071Component());
+    factory.registerComponent("true", new nts::Components::TrueComponent());
+    factory.registerComponent("false", new nts::Components::FalseComponent());
 }
 
 int main() {
