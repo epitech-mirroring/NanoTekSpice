b01a7322b10d65ac10ee48168ca1f6cb84408ae9
RenardFute <axel.eckenberg@yahoo.fr>
2024-02-27 13:00:04 +0100

    :loud_sound: Add even more debug

--- debug-coding-style.sh
+++ debug-coding-style.sh
@@ -2,5 +2,9 @@
 
 set -x  # Enable command tracing
 
+# Print the current directory and list its contents
+pwd
+ls -la
+
 # Call the coding-style.sh script with all the arguments passed to this script
 ./coding-style.sh "$@"
