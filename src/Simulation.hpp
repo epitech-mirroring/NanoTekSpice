b64301436bf065560e200ec3fcdecd8f2dc8e23a
RenardFute <axel.eckenberg@yahoo.fr>
2024-02-28 15:30:02 +0100

    :rewind: Re-add simulation.hpp that was deleted on merge

---
+++ src/Simulation.hpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2024
+** NanoTekSpice
+** File description:
+** Simulation
+*/
+
+#pragma once
+
+#include "FileContainer.hpp"
+
+namespace nts {
+    class Simulation {
+    public:
+        Simulation(std::map<std::string, IComponent *>);
+        ~Simulation();
+
+        void execSimulation();
+        void stopLoop(void);
+    protected:
+    private:
+        std::map<std::string, IComponent *> _pins;
+        std::unordered_map<std::string, void (nts::Simulation::*)()> _commands;
+        bool _exit;
+        size_t _ticks;
+        bool _loop;
+
+        bool isKnownCommand(char *line);
+        void exit(void);
+        void display(void);
+        void simulate(void);
+        void loop(void);
+
+        void handleInputs(char *line);
+        void setValues(std::string name, std::string value);
+    };
+};
