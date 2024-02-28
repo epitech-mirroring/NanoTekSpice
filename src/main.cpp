fd9e2248be34804fc00c089d081a4085713ceb80
RenardFute <axel.eckenberg@yahoo.fr>
2024-02-28 15:13:19 +0100

    :sparkles: Add components to main

--- src/main.cpp
+++ src/main.cpp
@@ -9,26 +9,36 @@
 #include "ComponentFactory.hpp"
 #include "components/InputComponent.hpp"
 #include "components/OrComponent.hpp"
-#include "components/Composed4071Component.hpp"
+#include "components/composed/C4071Component.hpp"
 #include "components/ClockComponent.hpp"
 #include "components/TrueComponent.hpp"
 #include "components/FalseComponent.hpp"
 #include "components/NotComponent.hpp"
 #include "components/AndComponent.hpp"
 #include "components/XorComponent.hpp"
+#include "components/composed/CNandComponent.hpp"
+#include "components/composed/CNorComponent.hpp"
+#include "components/composed/C4001Component.hpp"
+#include "components/composed/C4011Component.hpp"
+#include "components/composed/C4030Component.hpp"
 #include "Error.hpp"
 
 static void registerComponents(nts::ComponentFactory &factory)
 {
     factory.registerComponent("input", new nts::Components::InputComponent());
     factory.registerComponent("or", new nts::Components::OrComponent());
-    factory.registerComponent("4071", new nts::Components::Composed4071Component());
+    factory.registerComponent("4071", new nts::Components::C4071Component());
     factory.registerComponent("clock", new nts::Components::ClockComponent());
     factory.registerComponent("true", new nts::Components::TrueComponent());
     factory.registerComponent("false", new nts::Components::FalseComponent());
     factory.registerComponent("not", new nts::Components::NotComponent());
     factory.registerComponent("and", new nts::Components::AndComponent());
     factory.registerComponent("xor", new nts::Components::XorComponent());
+    factory.registerComponent("nand", new nts::Components::CNandComponent());
+    factory.registerComponent("nor", new nts::Components::CNorComponent());
+    factory.registerComponent("4001", new nts::Components::C4001Component());
+    factory.registerComponent("4011", new nts::Components::C4011Component());
+    factory.registerComponent("4030", new nts::Components::C4030Component());
 }
 
 int main(int argc, char **argv) {
