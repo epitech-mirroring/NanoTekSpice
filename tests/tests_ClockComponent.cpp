5895f4348dcf7b50687d0dd337a44be5ccf2eac7
Thomaltarix <thomas.boue@epitech.eu>
2024-03-01 10:57:57 +0100

    :white_check_mark: Fix the ClockComponent tests

--- tests/tests_ClockComponent.cpp
+++ tests/tests_ClockComponent.cpp
@@ -8,66 +8,44 @@
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 #include <iostream>
-#include "../src/components/OrComponent.hpp"
-#include "../src/components/TrueComponent.hpp"
-#include "../src/components/FalseComponent.hpp"
-#include "../src/components/InputComponent.hpp"
+#include "../src/components/ClockComponent.hpp"
 
 
-Test(OrComponent, simple_and)
+Test(ClockComponent, undefined)
 {
-    nts::Components::OrComponent andComp;
-    nts::Components::TrueComponent trueComp;
-    nts::Components::FalseComponent falseComp;
+    nts::Components::ClockComponent clockComp;
 
-    andComp.setLink(1, trueComp, 1);
-    andComp.setLink(2, falseComp, 1);
-    cr_assert_eq(andComp.compute(3), nts::Tristate::TRUE);
+    cr_assert_eq(clockComp.compute(1), nts::Tristate::UNDEFINED);
 }
 
-Test(OrComponent, simple_and2)
+Test(ClockComponent, simple_clock)
 {
-    nts::Components::OrComponent andComp;
-    nts::Components::TrueComponent trueComp;
-    nts::Components::TrueComponent trueComp2;
+    nts::Components::ClockComponent clockComp;
 
-    andComp.setLink(1, trueComp, 1);
-    andComp.setLink(2, trueComp2, 1);
-    cr_assert_eq(andComp.compute(3), nts::Tristate::TRUE);
+    clockComp.simulate(1);
+    cr_assert_eq(clockComp.compute(1), nts::Tristate::UNDEFINED);
 }
 
-Test(OrComponent, simple_and3)
+Test(ClockComponent, simple_clock2)
 {
-    nts::Components::OrComponent andComp;
-    nts::Components::FalseComponent falseComp;
-    nts::Components::FalseComponent falseComp2;
+    nts::Components::ClockComponent clockComp;
 
-    andComp.setLink(1, falseComp, 1);
-    andComp.setLink(2, falseComp2, 1);
-    cr_assert_eq(andComp.compute(3), nts::Tristate::FALSE);
+    clockComp.setValue(nts::Tristate::TRUE);
+    cr_assert_eq(clockComp.compute(1), nts::Tristate::TRUE);
+    clockComp.simulate(1);
+    cr_assert_eq(clockComp.compute(1), nts::Tristate::FALSE);
 }
 
-Test(OrComponent, not_linked)
+Test(ClockComponent, not_linked)
 {
-    nts::Components::OrComponent andComp;
+    nts::Components::ClockComponent clockComp;
 
-    cr_assert_eq(andComp.compute(3), nts::Tristate::UNDEFINED);
+    cr_assert_eq(clockComp.compute(1), nts::Tristate::UNDEFINED);
 }
 
-Test(OrComponent, undefined)
+Test(ClockComponent, clone)
 {
-    nts::Components::OrComponent andComp;
-    nts::Components::InputComponent inputComp;
-    nts::Components::InputComponent inputComp2;
+    nts::Components::ClockComponent clockComp;
 
-    andComp.setLink(1, inputComp, 1);
-    andComp.setLink(2, inputComp2, 1);
-    cr_assert_eq(andComp.compute(3), nts::Tristate::UNDEFINED);
-}
-
-Test(OrComponent, clone)
-{
-    nts::Components::OrComponent andComp;
-
-    cr_assert(andComp.clone() != nullptr);
+    cr_assert(clockComp.clone() != nullptr);
 }
