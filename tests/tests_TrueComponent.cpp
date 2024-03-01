ca7bb10230937f7504988a796d22a68ccb3ff017
Thomaltarix <thomas.boue@epitech.eu>
2024-03-01 10:58:37 +0100

    :white_check_mark: Fix the TrueComponent tests

--- tests/tests_TrueComponent.cpp
+++ tests/tests_TrueComponent.cpp
@@ -8,66 +8,19 @@
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 #include <iostream>
-#include "../src/components/OrComponent.hpp"
 #include "../src/components/TrueComponent.hpp"
-#include "../src/components/FalseComponent.hpp"
-#include "../src/components/InputComponent.hpp"
 
-
-Test(OrComponent, simple_and)
+Test(TrueComponent, simple_true)
 {
-    nts::Components::OrComponent andComp;
     nts::Components::TrueComponent trueComp;
-    nts::Components::FalseComponent falseComp;
 
-    andComp.setLink(1, trueComp, 1);
-    andComp.setLink(2, falseComp, 1);
-    cr_assert_eq(andComp.compute(3), nts::Tristate::TRUE);
+    cr_assert_eq(trueComp.compute(1), nts::Tristate::TRUE);
 }
 
-Test(OrComponent, simple_and2)
+Test(TrueComponent, clone)
 {
-    nts::Components::OrComponent andComp;
     nts::Components::TrueComponent trueComp;
-    nts::Components::TrueComponent trueComp2;
 
-    andComp.setLink(1, trueComp, 1);
-    andComp.setLink(2, trueComp2, 1);
-    cr_assert_eq(andComp.compute(3), nts::Tristate::TRUE);
-}
-
-Test(OrComponent, simple_and3)
-{
-    nts::Components::OrComponent andComp;
-    nts::Components::FalseComponent falseComp;
-    nts::Components::FalseComponent falseComp2;
-
-    andComp.setLink(1, falseComp, 1);
-    andComp.setLink(2, falseComp2, 1);
-    cr_assert_eq(andComp.compute(3), nts::Tristate::FALSE);
-}
-
-Test(OrComponent, not_linked)
-{
-    nts::Components::OrComponent andComp;
-
-    cr_assert_eq(andComp.compute(3), nts::Tristate::UNDEFINED);
-}
-
-Test(OrComponent, undefined)
-{
-    nts::Components::OrComponent andComp;
-    nts::Components::InputComponent inputComp;
-    nts::Components::InputComponent inputComp2;
-
-    andComp.setLink(1, inputComp, 1);
-    andComp.setLink(2, inputComp2, 1);
-    cr_assert_eq(andComp.compute(3), nts::Tristate::UNDEFINED);
-}
-
-Test(OrComponent, clone)
-{
-    nts::Components::OrComponent andComp;
-
-    cr_assert(andComp.clone() != nullptr);
+    std::unique_ptr<nts::IComponent> clone = trueComp.clone();
+    cr_assert_eq(clone->compute(1), nts::Tristate::TRUE);
 }
